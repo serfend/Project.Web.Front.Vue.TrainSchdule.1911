7496731a43b0cd8e4ad9d9cf5221bc55b2c19bdf
汉广 <liuchangm7j@vip.qq.com>
2020-04-07 22:31:08 +0800

    remove NProgress

--- src/permission.js
+++ src/permission.js
@@ -1,17 +1,14 @@
 import router from './router'
 import store from './store'
 // import { Message } from 'element-ui'
-import NProgress from 'nprogress' // progress bar
-import 'nprogress/nprogress.css' // progress bar style
+
 // import { getToken } from '@/utils/auth' // get token from cookie
 import getPageTitle from '@/utils/get-page-title'
 
-NProgress.configure({ showSpinner: false }) // NProgress Configuration
 // const whiteList = ['/login', '/auth-redirect'] // no redirect whitelist
 
-router.beforeEach(async(to, from, next) => {
+router.beforeEach(async (to, from, next) => {
   // start progress bar
-  NProgress.start()
   // set page title
   document.title = getPageTitle(to.meta.title)
   const { verify } = to.meta
@@ -41,5 +38,4 @@ router.beforeEach(async(to, from, next) => {
 
 router.afterEach(() => {
   // finish progress bar
-  NProgress.done()
 })
