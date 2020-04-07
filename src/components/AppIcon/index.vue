26775bf373db10a1479b6f72f26bd0c00085f769
汉广 <liuchangm7j@vip.qq.com>
2020-04-07 12:18:06 +0800

    new AppIcon初始化

---
+++ src/components/AppIcon/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <el-button>123</el-button>
+    <el-tag>AppName</el-tag>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'AppIcon'
+}
+</script>
+
+<style>
+
+</style>
