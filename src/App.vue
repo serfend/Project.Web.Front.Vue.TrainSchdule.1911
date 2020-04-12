268b4434d5a35698dc1dd0ec68224a5299af30d7
serfend <serfend@foxmail.com>
2020-04-13 01:06:15 +0800

    modefy block display scale notice

--- src/App.vue
+++ src/App.vue
@@ -12,23 +12,23 @@ export default {
     var scale = 1.25
     document.body.style.zoom = scale
 
-    this.$confirm(`当前屏幕已缩放到${scale}倍尺寸，是否保持?`, '确认尺寸', {
-      distinguishCancelAndClose: true,
-      confirmButtonText: '是',
-      cancelButtonText: '否'
-    })
-      .then(data => {
-        console.log(data)
-      })
-      .catch(action => {
-        console.log(action)
-        if (action === 'cancel') {
-          document.body.style.zoom = ''
-          this.$notify.info(
-            '您取消了页面缩放，之后也可以通过Ctrl+鼠标滚轮进行缩放'
-          )
-        }
-      })
+    // this.$confirm(`当前屏幕已缩放到${scale}倍尺寸，是否保持?`, '确认尺寸', {
+    //   distinguishCancelAndClose: true,
+    //   confirmButtonText: '是',
+    //   cancelButtonText: '否'
+    // })
+    //   .then(data => {
+    //     console.log(data)
+    //   })
+    //   .catch(action => {
+    //     console.log(action)
+    //     if (action === 'cancel') {
+    //       document.body.style.zoom = ''
+    //       this.$notify.info(
+    //         '您取消了页面缩放，之后也可以通过Ctrl+鼠标滚轮进行缩放'
+    //       )
+    //     }
+    //   })
   }
 }
 </script>
