cd9205b95cf65861b7c6cfd888708db689330f18
serfend <serfend@foxmail.com>
2020-04-29 19:38:09 +0800

    modefy remove prevYearlyLength

--- src/views/register/components/social.vue
+++ src/views/register/components/social.vue
@@ -4,14 +4,6 @@
     <SettleFormItem :form.sync="innerForm.settle.lover" label="配偶居住地" :disabled="false" />
     <SettleFormItem :form.sync="innerForm.settle.parent" label="本人父母居住地" :disabled="false" />
     <SettleFormItem :form.sync="innerForm.settle.loversParent" label="配偶父母居住地" :disabled="false" />
-    <el-form-item label="初始休假天数">
-      <el-col :lg="8">
-        <el-input-number v-model="innerForm.settle.prevYearlyLength" />
-        <el-tooltip content="填写今年1月1日时应休正休假天数，详细政策同《人力知会手册》" effect="dark" placement="bottom">
-          <i class="el-icon-question" />
-        </el-tooltip>
-      </el-col>
-    </el-form-item>
     <el-col :span="8">
       <el-form-item
         label="联系方式"
@@ -47,8 +39,7 @@ export default {
           self: {},
           lover: {},
           parent: {},
-          loversParent: {},
-          prevYearlyLength: 0
+          loversParent: {}
         }
       }
     }
