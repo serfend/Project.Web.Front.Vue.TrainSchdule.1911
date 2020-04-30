da041af3af60c07c0e268efc0cb52c7d279d9444
serfend <serfend@foxmail.com>
2020-04-30 16:09:40 +0800

    modefy rebuild settleFormItem

--- src/components/SettleFormItem/index.vue
+++ src/components/SettleFormItem/index.vue
@@ -1,45 +1,51 @@
 <template>
   <el-form-item :label="label">
-    <el-tooltip content="无此项（例如未婚、离婚、去世）时则将此项禁用">
-      <el-switch
-        v-model="innerForm.valid"
-        active-text="启用"
-        inactive-text="禁用"
-        :disabled="disabled"
-      />
-    </el-tooltip>
-    <el-row :gutter="8">
-      <el-col :span="12">
-        <el-input v-model="innerForm.addressDetail" :disabled="disabled" placeholder="无信息">
-          <div slot="prepend">详细地址</div>
-        </el-input>
-      </el-col>
-      <el-col :span="4">
-        <el-tooltip content="生效时间，表示最后一次更换地点的时间">
-          <el-date-picker
-            v-model="innerForm.date"
-            :disabled="disabled"
-            placeholder="无信息"
-            format="yyyy年MM月dd"
-            value-format="yyyy-MM-dd"
-          />
-        </el-tooltip>
-      </el-col>
-      <el-col :span="6">
-        <el-tooltip content="行政区划,填到区/县一级">
-          <cascader-selector
-            :placeholder="innerForm.address.name"
-            :disabled="disabled"
-            :code.sync="innerForm.address.code"
-            :child-getter-method="locationChildren"
-          />
-        </el-tooltip>
-      </el-col>
-    </el-row>
+    <div v-if="!disabled">
+      <el-tooltip content="无此项（例如未婚、离婚、去世）时则将此项禁用">
+        <el-switch v-model="innerForm.valid" active-text="启用" inactive-text="禁用" />
+      </el-tooltip>
+      <el-row type="flex" justify="start" :gutter="8">
+        <el-col :lg="6" :md="8" :sm="12" :xs="24">
+          <el-tooltip content="生效时间，表示最后一次更换地点的时间">
+            <el-date-picker
+              v-model="innerForm.date"
+              placeholder="未填写日期"
+              format="yyyy年MM月dd日"
+              value-format="yyyy-MM-dd"
+            />
+          </el-tooltip>
+        </el-col>
+        <el-col :lg="8" :md="16" :sm="12" :xs="24">
+          <el-tooltip content="行政区划,填到区/县一级">
+            <cascader-selector
+              :placeholder="innerForm.address.name"
+              :code.sync="innerForm.address.code"
+              :child-getter-method="locationChildren"
+            />
+          </el-tooltip>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :lg="12" :md="24">
+          <el-input v-model="innerForm.addressDetail" placeholder="未填写详细地址">
+            <div slot="prepend">详细地址</div>
+          </el-input>
+        </el-col>
+      </el-row>
+    </div>
+    <div v-else>
+      <div v-if="!innerForm.valid">无</div>
+      <div v-else>
+        <el-tag>{{ parseTime(new Date(innerForm.date),'{yyyy}年{mm}月{dd}日') }}</el-tag>
+        <span style="font-size:1.1em">{{ innerForm.address.name }}</span>
+        <span style="color:#aaaaaa;font-size:1em">{{ innerForm.addressDetail }}</span>
+      </div>
+    </div>
   </el-form-item>
 </template>
 
 <script>
+import { parseTime } from '@/utils'
 import CascaderSelector from '@/components/CascaderSelector'
 import { locationChildren } from '@/api/static'
 export default {
@@ -101,6 +107,7 @@ export default {
     }
   },
   methods: {
+    parseTime,
     locationChildren,
     createInnerForm() {
       return {
