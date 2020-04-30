57f00d597a5e6a44f8a046a71ce6a81515a08f08
serfend <serfend@foxmail.com>
2020-04-30 21:05:39 +0800

    modefy usercard support qrcode

--- src/components/User/index.vue
+++ src/components/User/index.vue
@@ -1,29 +1,41 @@
 <template>
-  <el-card style="width:250px">
-    <el-row v-if="innerData">
-      <el-col :span="8">
-        <el-avatar :size="60" :src="avatar" />
-      </el-col>
-      <el-col :span="16">
-        <h2>{{ innerData.realName }}</h2>
-        <div style="color:#cccccc">{{ innerData.id }}</div>
-        <div style="color:#8f8f8f">{{ innerData.about }}</div>
+  <el-card :style="{width:width}">
+    <el-container v-if="innerData">
+      <el-aside width="100px" style="padding:0;margin:0;background: rgb(255, 255, 255);">
+        <el-popover :placement="direction" trigger="hover" @show="loadContactMe">
+          <ContactMe v-if="contactMeHasShow" :content="contactUrl" />
+          <div slot="reference">
+            <el-avatar :size="100" :src="avatar" />
+          </div>
+        </el-popover>
         <el-tag
-          :style="{ 'background-color':innerData.gender==2?'#ee6666':'#60c3e9',color:'#ffffff' }"
+          :style="{ 'background-color':innerData.gender==2?'#ee6666':'#60c3e9',color:'#ffffff',display:'grid','justify-items':'center'}"
           size="mini"
         >
-          <div>{{ innerData.companyName }}{{ innerData.dutiesName }}</div>
+          <div>{{ innerData.dutiesName }}</div>
         </el-tag>
-      </el-col>
-    </el-row>
-    <el-row v-else style="color:#888888;font-size:0.5em">加载中...</el-row>
+      </el-aside>
+
+      <el-main style="padding:0;margin:0;background: rgb(255, 255, 255);">
+        <div style="padding-left:10px">
+          <h2>{{ innerData.realName }}</h2>
+          <div style="color:#cccccc">{{ innerData.id }}</div>
+          <div style="color:#8f8f8f">{{ innerData.about }}</div>
+          <div>{{ innerData.companyName }}</div>
+        </div>
+      </el-main>
+    </el-container>
+
+    <div v-else style="color:#888888;font-size:0.5em">加载中...</div>
   </el-card>
 </template>
 
 <script>
-import { getUserAvatar } from '@/api/userinfo'
+import ContactMe from '@/components/ContactMe'
+import { getUserAvatar, getUserSocial } from '@/api/userinfo'
 export default {
   name: 'Duty',
+  components: { ContactMe },
   props: {
     data: {
       type: Object,
@@ -36,13 +48,28 @@ export default {
     canLoadAvatar: {
       type: Boolean,
       default: false
+    },
+    direction: {
+      type: String,
+      default: 'top'
+    },
+    width: {
+      type: String,
+      default: '350px'
     }
   },
   data() {
     return {
       avatar: '',
       userid: '',
-      innerData: {}
+      phone: '',
+      innerData: {},
+      contactMeHasShow: false
+    }
+  },
+  computed: {
+    contactUrl() {
+      return `MECARD:TEL:${this.phone};N:${this.innerData.realName};EMAIL:${this.innerData.id}@xjxt.mtn;NOTE:${this.innerData.about};`
     }
   },
   watch: {
@@ -71,10 +98,17 @@ export default {
     this.refreshAvatar()
   },
   methods: {
+    loadContactMe() {
+      if (this.contactMeHasShow) return
+      this.contactMeHasShow = true
+    },
     refreshAvatar() {
       if (!this.canLoadAvatar) return
       getUserAvatar(this.userid).then(data => {
         this.avatar = data.url
+      })
+      getUserSocial(this.userid).then(data => {
+        this.phone = data.phone
       })
     }
   }
