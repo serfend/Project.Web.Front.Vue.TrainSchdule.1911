97433e11b54998fadb1bc8949556f0bc2ad748ba
serfend <serfend@foxmail.com>
2020-05-01 22:33:44 +0800

    new qrcode  analysis page

--- src/views/common/QrCodeScan/index.vue
+++ src/views/common/QrCodeScan/index.vue
@@ -1,40 +1,78 @@
 <template>
   <el-card header="二维码生成">
-    <el-upload
-      :action="avatarSubmitUrl"
-      :before-upload="beforeAvatarUpload"
-      class="avatar-uploader"
-      :show-file-list="false"
-    />
+    <el-form label-width="60px">
+      <el-container>
+        <el-aside width="400px" style="background:#fff">
+          <el-form-item label="内容">
+            <el-input v-model="file.data" type="textarea" autosize />
+          </el-form-item>
+          <el-form-item label="二维码">
+            <ContactMe :content="file.data" description />
+          </el-form-item>
+        </el-aside>
+        <el-main>
+          <el-form-item>
+            <el-upload
+              drag
+              :before-upload="beforeAvatarUpload"
+              :on-success="onUploadSuccess"
+              :action="uploadurl"
+              :data="file"
+            >
+              <i class="el-icon-upload" />
+              <div class="el-upload__text">
+                将文件拖到此处，或
+                <em>点击上传</em>
+              </div>
+            </el-upload>
+          </el-form-item>
+          <el-form-item style="width:420px">
+            <el-image :src="file.imgraw" />
+          </el-form-item>
+        </el-main>
+      </el-container>
+    </el-form>
   </el-card>
 </template>
 
 <script>
+import ContactMe from '@/components/ContactMe'
+import { qrCodeScan } from '@/api/qrCode'
 export default {
   name: 'QrCodeScan',
+  components: { ContactMe },
   data() {
     return {
-      imgSubmitUrl: ''
+      uploadurl: '',
+      file: {
+        img: '',
+        imgraw: '',
+        data: ''
+      }
     }
-  },
-  mounted() {
-    this.imgSubmitUrl = process.env.VUE_APP_BASE_API + '/static/QrCodeScan'
   },
   methods: {
     beforeAvatarUpload(file) {
-      const isLt2M = file.size / 1024 / 1024 / 1024 < 200
-      if (!isLt2M) {
-        this.$message.error('上传头像图片大小不能超过 200KB')
+      const cfile = this.file
+      var self = this
+      var reader = new FileReader()
+      reader.onload = function(evt) {
+        cfile.imgraw = evt.target.result
+        cfile.img = cfile.imgraw.substring('data:image/jpeg;base64,'.length)
+        qrCodeScan(cfile.img).then(data => {
+          cfile.data = data.data
+          self.$message.success(`加载${file.name}成功`)
+        })
       }
-      if (isLt2M) {
-        var reader = new FileReader()
-        reader.onload = function(evt) {
-          var base64 = evt.target.result
-          console.log(base64)
-        }
-        reader.readAsDataURL(file)
-      }
+      reader.readAsDataURL(file)
       return false
+    },
+    onUploadSuccess(data, status, arr) {
+      if (data.status !== 0) {
+        this.$message.error(data.message)
+      } else {
+        this.$message.success(`${status.name}识别成功`)
+      }
     }
   }
 }
