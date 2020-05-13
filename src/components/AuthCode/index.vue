bfaff52971f3b4c15c0bc2467cb066cee545d988
serfend <serfend@foxmail.com>
2020-05-13 18:18:38 +0800

    modefy authcode show

--- src/components/AuthCode/index.vue
+++ src/components/AuthCode/index.vue
@@ -5,44 +5,60 @@
         <div v-if="$store.state.user.name">
           <el-tooltip>
             <template slot="content">默认为当前登录</template>
-            <el-tag>{{ $store.state.user.name }}</el-tag>
+            <el-tag type="success">{{ $store.state.user.name }}</el-tag>
           </el-tooltip>
         </div>
         <span v-else style="color:#ff8f8f">未登录,请使用授权码</span>
 
         <el-tooltip placement="top" effect="light">
           <div slot="content">
-            授权码是用于敏感操作认证的密钥
-            <el-row />请手机下载身份验证器
-            <el-tooltip effect="light">
-              <div slot="content">
-                身份验证器下载:
-                <el-row />
-                <el-image :src="apkImage" style="width:200px" />
-              </div>
-              <i class="el-icon-info blue--text" />
-            </el-tooltip>
-            <div v-if="authKeyUrl">
-              <el-row />后扫描此码以获取密钥
-              <el-row />
-              <el-image :src="authKeyUrl" style="width:200px" />
+            <div>授权码是用于敏感操作认证的密钥</div>
+            <div>
+              <el-popover placement="top" trigger="hover" @show="downloadAutherHasShow = true">
+                <ContactMe
+                  v-if="downloadAutherHasShow"
+                  content="双重验证码"
+                  description="搜索微信小程序【双重验证码】"
+                />
+                <el-link slot="reference" type="primary">获取身份验证器</el-link>
+              </el-popover>
+            </div>
+            <div>
+              <el-popover placement="top" trigger="hover" @show="authKeyUrlHasShow = true">
+                <ContactMe
+                  v-if="authKeyUrlHasShow"
+                  :content="authKeyUrl"
+                  description="请使用身份验证器扫描此码（仅首次需要）"
+                />
+                <el-link slot="reference" type="primary">获取当前账号授权码</el-link>
+              </el-popover>
+            </div>
+            <div v-if="$store.state.user.name">
+              <el-tag type="success">已登录无需使用授权码哦~</el-tag>
             </div>
             <div v-else>
               <el-alert title="当前未登录,登录后显示授权码" type="error" center />
             </div>
           </div>
           <el-tag type="info" style="margin:0 0 0 20px">
-            什么是授权码
+            <span>什么是授权码</span>
             <i class="el-icon-info blue--text" />
           </el-tag>
         </el-tooltip>
       </template>
       <el-form>
         <el-form-item label="授权人">
-          <el-input v-model="innerForm.authByUserId" placeholder="请输入授权人的账户" />
+          <el-popover @show="forgetHasShow=true">
+            <FindUserByRealName v-if="forgetHasShow" />
+            <el-input
+              slot="reference"
+              v-model="innerForm.authByUserId"
+              placeholder="请输入授权人的账户（不是姓名）"
+            />
+          </el-popover>
         </el-form-item>
-        <el-form-item label="安全码">
-          <el-input v-model="innerForm.code" placeholder="请输入安全码" @change="checkCode">
+        <el-form-item label="授权码">
+          <el-input v-model="innerForm.code" placeholder="请输入授权码" @change="checkCode">
             <template slot="suffix">
               <el-tooltip :content="invalid.code.des">
                 <i v-if="invalid.code.status" class="el-icon-error" style="color:#F56C6C" />
@@ -57,11 +73,13 @@
 </template>
 
 <script>
-import apkImage from '@/assets/jpg/apk.jpg'
-
+import FindUserByRealName from '@/views/ForgetPassword/FindUserByRealName'
+import ContactMe from '@/components/ContactMe'
 import { getAuthKey, checkAuthCode } from '@/api/account'
+import { getUserIdByCid } from '@/api/userinfo'
 export default {
   name: 'AuthCode',
+  components: { ContactMe, FindUserByRealName },
   props: {
     form: {
       type: Object,
@@ -78,8 +96,10 @@ export default {
   },
   data() {
     return {
+      forgetHasShow: false,
+      downloadAutherHasShow: false,
+      authKeyUrlHasShow: false,
       authKeyUrl: null,
-      apkImage: apkImage,
       innerForm: {
         authByUserId: null,
         code: null
@@ -95,9 +115,21 @@ export default {
   watch: {
     innerForm: {
       handler(val, oldVal) {
-        if (val.authByUserId === '') val.authByUserId = null
         if (val.code === '') val.code = null
-        this.$emit('update:form', val)
+        if (!val.authByUserId) {
+          val.authByUserId = null
+        } else if (val.authByUserId.length === 18) {
+          getUserIdByCid(val.authByUserId, true)
+            .then(data => {
+              val.authByUserId = data.id
+              this.$message.success(`成功转换身份证号为id`)
+            })
+            .finally(() => {
+              this.$emit('update:form', val)
+            })
+        } else {
+          this.$emit('update:form', val)
+        }
       },
       deep: true
     }
@@ -109,7 +141,7 @@ export default {
   methods: {
     getAuthKeyImg() {
       getAuthKey(true).then(r => {
-        if (r.url) this.authKeyUrl = 'data:image/png;base64,' + r.url
+        if (r.url) this.authKeyUrl = r.url
         if (
           !this.$store.state.user.data.isInitPassword &&
           this.$store.state.user.userid !== ''
@@ -123,8 +155,7 @@ export default {
     },
     checkCode() {
       var fn = this.authCheckMethod
-      var p = fn(this.innerForm.authByUserId, this.innerForm.code, true)
-      p(this.innerForm.authByUserId, this.innerForm.code, true)
+      fn(this.innerForm.authByUserId, this.innerForm.code, true)
         .then(() => {
           this.$emit('update:status', true)
           this.invalid.code.status = false
