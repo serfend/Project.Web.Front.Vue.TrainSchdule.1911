aac68f1f5b377ef544132b3ac18759aead49e1bf
serfend <serfend@foxmail.com>
2020-05-19 19:49:07 +0800

    feat[user-card]layout support preview

--- src/components/User/index.vue
+++ src/components/User/index.vue
@@ -1,39 +1,40 @@
 <template>
   <el-card :style="{width:innerData&&innerData.id?width:'100%'}">
     <el-container v-if="innerData&&innerData.id">
-      <el-aside width="100px" style="padding:0;margin:0;background: rgb(255, 255, 255);">
-        <el-popover :placement="direction" trigger="hover" @show="loadContactMe">
-          <ContactMe
-            v-if="contactMeHasShow"
-            :content="contactUrl"
-            :description="`微信或手机通讯录扫码，获取${innerData.realName}的联系方式`"
-          />
-          <div slot="reference">
-            <el-avatar :size="100" :src="avatar" />
-          </div>
-        </el-popover>
+      <el-aside
+        width="100px"
+        style="padding:0;margin:0;display:grid;justify-content:center;background:#0000"
+      >
+        <el-image :src="avatar" :preview-src-list="[avatar]" class="avatar" />
         <el-tag
-          :style="{ 'background-color':innerData.gender==2?'#ee6666':'#60c3e9',color:'#ffffff',display:'grid','justify-items':'center'}"
+          :style="{ 'background-color':innerData.gender==2?'#ee6666':'#60c3e9',color:'#ffffff',display:'grid','justify-items':'center','margin-top':'1em'}"
           size="mini"
         >
           <div>{{ innerData.dutiesName }}</div>
         </el-tag>
       </el-aside>
 
-      <el-main class="miniForm">
-        <el-form label-width="3em" size="mini">
-          <h2>{{ innerData.realName }}</h2>
-          <el-form-item label="id">
-            <div style="color:#cccccc">{{ innerData.id }}</div>
-          </el-form-item>
-          <el-form-item v-if="innerData.about" label="关于">
-            <div style="color:#8f8f8f">{{ innerData.about }}</div>
-          </el-form-item>
-          <el-form-item label="单位">
-            <div>{{ innerData.companyName }}</div>
-          </el-form-item>
-        </el-form>
-      </el-main>
+      <el-popover :placement="direction" trigger="hover" @show="loadContactMe">
+        <ContactMe
+          v-if="contactMeHasShow"
+          :content="contactUrl"
+          :description="`微信或手机通讯录扫码，获取${innerData.realName}的联系方式`"
+        />
+        <el-main slot="reference" class="miniForm">
+          <el-form label-width="3em" size="mini">
+            <h2>{{ innerData.realName }}</h2>
+            <el-form-item label="id">
+              <div style="color:#cccccc">{{ innerData.id }}</div>
+            </el-form-item>
+            <el-form-item v-if="innerData.about" label="关于">
+              <div style="color:#8f8f8f">{{ innerData.about }}</div>
+            </el-form-item>
+            <el-form-item label="单位">
+              <div>{{ innerData.companyName }}</div>
+            </el-form-item>
+          </el-form>
+        </el-main>
+      </el-popover>
     </el-container>
 
     <div v-else style="color:#888888;font-size:1em">加载中...</div>
@@ -137,5 +138,12 @@ export default {
       margin-bottom: 0px;
     }
   }
+}
+.avatar {
+  transition: all 0.5s ease;
+  cursor: pointer;
+  width: 4.5em;
+  height: 4.5em;
+  border-radius: 50%;
 }
 </style>
