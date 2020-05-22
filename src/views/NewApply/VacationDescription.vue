151f955edf4c67bef1cb57fc4858fd956df99154
serfend <serfend@foxmail.com>
2020-05-22 20:22:31 +0800

    refactor[vacation-description]split

--- src/views/NewApply/VacationDescription.vue
+++ src/views/NewApply/VacationDescription.vue
@@ -1,40 +1,17 @@
 <template>
-  <el-tooltip>
-    <div slot="content" class="tooltip-vacation">
-      <ul>
-        <li>
-          <b class="bolder">全年假期长度：</b>
-          <span class="text-orange">{{ usersVacation.yearlyLength }}</span>天
-        </li>
-        <li>
-          <b class="bolder">当前已休次数：</b>
-          <span class="text-orange">{{ usersVacation.nowTimes }}</span>次
-        </li>
-        <li>
-          <b class="bolder">剩余假期长度：</b>
-          <span class="text-orange">{{ usersVacation.leftLength }}</span>天
-        </li>
-        <li>
-          <b class="bolder">全年最多可休路途次数：</b>
-          <span class="text-orange">{{ usersVacation.maxTripTimes }}</span>次
-        </li>
-        <li>
-          <b class="bolder">当前已休路途次数:</b>
-          <span class="text-orange">{{ usersVacation.onTripTimes }}</span>次
-        </li>
-        <li>
-          <b class="bolder">休假描述:</b>
-          <span>{{ usersVacation.description || `暂无说明` }}</span>
-        </li>
-      </ul>
-    </div>
+  <el-tooltip effect="light">
+    <VacationDescriptionContent slot="content" :users-vacation="usersVacation" />
     <el-progress :percentage="caculatevacationPercentage()" />
   </el-tooltip>
 </template>
 
 <script>
+import VacationDescriptionContent from './VacationDescriptionContent'
 export default {
   name: 'VacationDescription',
+  components: {
+    VacationDescriptionContent
+  },
   props: {
     usersVacation: {
       type: Object,
@@ -53,10 +30,10 @@ export default {
       var fn = parseInt
       var result = Math.floor(
         100 *
-          ((fn(this.usersVacation.yearlyLength) -
-            fn(this.usersVacation.leftLength) +
-            fn(this.thisTimeVacationLength)) /
-            fn(this.usersVacation.yearlyLength))
+        ((fn(this.usersVacation.yearlyLength) -
+          fn(this.usersVacation.leftLength) +
+          fn(this.thisTimeVacationLength)) /
+          fn(this.usersVacation.yearlyLength))
       )
       if (result < 0) result = 0
       if (result > 100) result = 100
@@ -65,19 +42,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.tooltip-vacation {
-  width: 260px;
-  ul,
-  li {
-    list-style: none;
-    padding: 8px;
-    letter-spacing: 1px;
-
-    .text-orange {
-      color: orange;
-    }
-  }
-}
-</style>
