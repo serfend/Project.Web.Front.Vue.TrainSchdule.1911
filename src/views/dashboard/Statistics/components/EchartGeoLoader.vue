073a408423378b9d4c48e31f1d62652740315e07
serfend <serfend@foxmail.com>
2020-05-24 13:24:19 +0800

    feat[dashboard]data driver

---
+++ src/views/dashboard/Statistics/components/EchartGeoLoader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-loading="loading" class="display-item">geo data loader</div>
+</template>
+<script>
+import echarts from 'echarts'
+export default {
+  name: 'StatisticsDataDriver',
+  props: {
+    fileLoad: {
+      type: Function,
+      default: null
+    }
+  },
+  data: () => ({
+    loading: false
+  }),
+  methods: {
+    async refresh() {
+      if (this.fileLoad == null) return this.$message.error('配置获取失败')
+      var geoJson = await this.fileLoad('china-200524.json')
+      echarts.registerMap('china', geoJson)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.display-item {
+  color: #aaa;
+  font-size: 0.3em;
+}
+</style>
