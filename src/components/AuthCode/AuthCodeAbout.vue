79f5cf42f506fbd6c03859726b8ed50cb19e8d05
serfend <serfend@foxmail.com>
2020-05-25 23:37:27 +0800

    feat[auth-code]use another app

--- src/components/AuthCode/AuthCodeAbout.vue
+++ src/components/AuthCode/AuthCodeAbout.vue
@@ -1,20 +1,31 @@
 <template>
-  <el-tooltip placement="top" effect="light">
-    <div slot="content">
+  <el-popover placement="top" trigger="click">
+    <div>
       <div>授权码是用于敏感操作认证的密钥</div>
       <div>
-        <el-popover placement="top" trigger="hover" @show="downloadAutherHasShow = true">
-          <ContactMe v-if="downloadAutherHasShow" content="双重验证码" description="搜索微信小程序【双重验证码】" />
+        <el-popover placement="top" trigger="click" @show="downloadAutherHasShow = true">
+          <div style="font-weight:600;align-text:center">
+            使用微信扫码获取小程序
+            <div>小程序名称：二次验证码</div>
+            <div style="color:#f00">非官方软件，请勿充值</div>
+            <el-image :src="totpImg" />
+          </div>
           <el-link slot="reference" type="primary">获取身份验证器</el-link>
         </el-popover>
       </div>
       <div v-if="authKeyUrl">
-        <el-popover placement="top" trigger="hover" @show="authKeyUrlHasShow = true">
+        <el-popover placement="top" trigger="click" @show="authKeyUrlHasShow = true">
           <ContactMe
             v-if="authKeyUrlHasShow"
             :content="authKeyUrl"
             description="请使用身份验证器扫描此码（仅首次需要）"
           />
+          <el-button
+            type="info"
+            icon="el-icon-document-copy"
+            size="mini"
+            @click="clipBoard(authKeyUrl,$event)"
+          >复制密钥链接</el-button>
           <el-link slot="reference" type="primary">获取当前账号授权码</el-link>
         </el-popover>
       </div>
@@ -25,29 +36,33 @@
         <el-alert title="当前未登录,登录后显示授权码" type="error" center />
       </div>
     </div>
-    <el-tag type="info" style="margin:0 0 0 20px">
+    <el-tag slot="reference" type="info" style="margin:0 0 0 20px">
       <span>什么是授权码</span>
       <i class="el-icon-info blue--text" />
     </el-tag>
-  </el-tooltip>
+  </el-popover>
 </template>
 
 <script>
+import clipBoard from '@/utils/clipboard'
 import { getAuthKey } from '@/api/account'
 import ContactMe from '@/components/ContactMe'
-
+import totp from '@/assets/jpg/app/totp.jpg'
 export default {
   name: 'AuthCodeAbout',
   components: { ContactMe },
   data: () => ({
     downloadAutherHasShow: false,
     authKeyUrlHasShow: false,
-    authKeyUrl: null
+    authKeyUrl: null,
+    totpImg: ''
   }),
   mounted() {
     this.getAuthKeyImg()
+    this.totpImg = totp
   },
   methods: {
+    clipBoard,
     getAuthKeyImg() {
       getAuthKey(true).then(r => {
         if (r.url) this.authKeyUrl = r.url
