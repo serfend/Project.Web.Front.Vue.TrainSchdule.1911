e4792c259402033805dcd3a38559f3c4c4c6bd74
serfend <serfend@foxmail.com>
2020-06-12 12:41:43 +0800

    feat[components]setting support load string

--- src/views/dashboard/Statistics/components/Engine/SettingEngine.vue
+++ src/views/dashboard/Statistics/components/Engine/SettingEngine.vue
@@ -6,19 +6,24 @@
       <el-alert v-else type="error">设置选项未初始化</el-alert>
       <template slot="footer">
         <el-button type="success" icon="el-icon-upload" @click="saveSetting">保存</el-button>
-        <el-button type="danger" icon="el-icon-download" @click="reloadPage">加载</el-button>
+        <el-button type="danger" icon="el-icon-refresh" @click="reloadPage">加载</el-button>
+        <el-button type="info" icon="el-icon-edit" @click="editSetting">高级编辑</el-button>
         <el-select v-model="settingName" allow-create filterable>
           <el-option v-for="item in settings" :key="item" :value="item" :label="item" />
         </el-select>
       </template>
+      <el-dialog :visible.sync="showSettingEditDialog" append-to-body>
+        <el-input v-model="settingText" type="textarea" autosize />
+        <el-button type="success" style="width:100%" @click="saveEditSetting">保存</el-button>
+      </el-dialog>
     </el-dialog>
   </div>
 </template>
 
 <script>
-import { saveSetting, loadSetting } from './setting'
 import MagicForm from '@/components/MagicForm'
 import { debounce } from '@/utils'
+import { loadSettingString } from '@/store/modules/dashboard/index'
 export default {
   name: 'SettingEngine',
   components: { MagicForm },
@@ -32,15 +37,27 @@ export default {
   },
   data: () => ({
     showDialog: false,
+    showSettingEditDialog: false,
     innerSetting: null,
     settingName: 'default',
     settings: ['default'],
-    lastUpdate: new Date()
+    lastUpdate: new Date(),
+    settingText: null
   }),
   computed: {
     updatedSetting() {
       return debounce(() => {
         this.$emit('update:setting', this.innerSetting)
+      }, 500)
+    },
+    requireSaveConfig() {
+      return debounce(() => {
+        return this.saveConfig()
+      }, 500)
+    },
+    updatedSettingName() {
+      return debounce(() => {
+        return this.loadSetting()
       }, 500)
     }
   },
@@ -48,15 +65,16 @@ export default {
     settings: {
       handler(val) {
         this.$nextTick(() => {
-          this.saveConfig()
+          this.requireSaveConfig()
         })
       }
     },
     settingName: {
       handler(val) {
         this.$nextTick(() => {
-          loadSetting(val)
-          this.saveConfig()
+          this.updatedSettingName().then(() => {
+            this.requireSaveConfig()
+          })
         })
       }
     },
@@ -81,20 +99,48 @@ export default {
       var item = JSON.parse(rawSetting)
       this.settings = item.settings
       this.settingName = item.name
-      loadSetting(this.settingName)
+      this.$nextTick(() => this.updatedSettingName())
     }
   },
   methods: {
     showSetting() {
       this.showDialog = true
     },
+    loadSetting() {
+      return new Promise((res, rej) => {
+        this.$store
+          .dispatch('dashboard/loadSetting', { name: this.settingName })
+          .catch(() => {
+            this.$store.dispatch('dashboard/init')
+          })
+          .finally(() => {
+            res()
+          })
+      })
+    },
     saveSetting() {
-      saveSetting(this.settingName, this.innerSetting)
+      this.$store.dispatch('dashboard/saveSetting', {
+        name: this.settingName,
+        setting: this.innerSetting
+      })
+      this.$message.success('已保存')
     },
     reloadPage() {
       this.$confirm('重新加载设置将刷新页面').then(() => {
         this.$router.go(0)
       })
+    },
+    saveEditSetting() {
+      const item = JSON.parse(this.settingText)
+      this.$store.dispatch('dashboard/saveSetting', {
+        name: this.settingName,
+        setting: item
+      })
+      this.$message.success('已保存')
+    },
+    editSetting() {
+      this.settingText = loadSettingString(this.settingName)
+      this.showSettingEditDialog = true
     },
     saveConfig() {
       if (this.settings.indexOf(this.settingName) === -1) {
