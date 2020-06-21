aec70a68b5c146e22a0952a3801155391cdfbf88
serfend <serfend@foxmail.com>
2020-06-21 12:49:08 +0800

    fix[vacation-desc]show color rank

--- src/components/Vacation/VacationDescription.vue
+++ src/components/Vacation/VacationDescription.vue
@@ -1,11 +1,12 @@
 <template>
   <el-tooltip effect="light">
     <VacationDescriptionContent slot="content" :users-vacation="usersVacation" />
-    <el-progress :percentage="caculatevacationPercentage()" />
+    <el-progress :percentage="percent" :color="getColor(percent)" />
   </el-tooltip>
 </template>
 
 <script>
+import { rgbToHex } from '@/utils'
 import VacationDescriptionContent from './VacationDescriptionContent'
 export default {
   name: 'VacationDescription',
@@ -24,8 +25,8 @@ export default {
       default: 0
     }
   },
-  methods: {
-    caculatevacationPercentage() {
+  computed: {
+    percent() {
       if (!this.usersVacation.yearlyLength) return 100
       var uv = this.usersVacation
       const vl = this.thisTimeVacationLength
@@ -35,6 +36,14 @@ export default {
       if (result < 0) result = 0
       if (result > 100) result = 100
       return result
+    }
+  },
+  methods: {
+    getColor(percent) {
+      const r = Math.round((100 - percent) * 2.55)
+      const g = Math.round(percent * 2.55)
+      const c = rgbToHex(`rgba(${r},${g},0,255)`)
+      return c
     }
   }
 }
