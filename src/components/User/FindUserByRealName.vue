4a6fbd7102138934148dfab836188fd1392c87eb
serfend <serfend@foxmail.com>
2020-06-27 21:12:20 +0800

    feat[user-select]show loader

--- src/components/User/FindUserByRealName.vue
+++ src/components/User/FindUserByRealName.vue
@@ -6,53 +6,67 @@
       placeholder="输入姓名查找"
       @input="handleUserSelectByRealnameChange"
     />
-    <el-collapse v-model="nowCollapseSelectUserId" accordion @change="selectUserChange">
-      <el-collapse-item v-for="u in usersByRealName" :key="u.id" :name="u.id">
-        <template slot="title">
-          <el-tag>{{ u.dutiesName }}</el-tag>
-          {{ u.companyName }} {{ u.realName }}
-        </template>
-        <User :data="u" :can-load-avatar="u.canLoadAvatar" />
-      </el-collapse-item>
-    </el-collapse>
+    <div v-loading="loading">
+      <el-collapse v-model="nowCollapseSelectUserId" accordion @change="selectUserChange">
+        <el-collapse-item v-for="(u,index) in usersByRealName" :key="u.id" :name="u.id">
+          <template slot="title">
+            <el-tag>{{ u.dutiesName }}</el-tag>
+            {{ u.companyName }} {{ u.realName }}
+          </template>
+          <User
+            :data="u"
+            :can-load-avatar="u.canLoadAvatar"
+            :avatar.sync="usersByRealName[index].avatar"
+            @avatar-load="avatarLoad(index)"
+          />
+        </el-collapse-item>
+      </el-collapse>
+      <el-button v-show="hasNextPage" style="width:100%" @click="onNextPage">加载更多...</el-button>
+    </div>
   </div>
 </template>
 
 <script>
 import User from '@/components/User'
 import { getUserIdByRealName } from '@/api/user/userinfo'
+import { debounce } from '@/utils'
 export default {
   name: 'FindUserByRealName',
   components: { User },
   data: () => ({
+    loading: false,
     nowSelectRealName: '',
+    nowIndex: 0,
     usersByRealName: [],
     userDict: {},
-    nowCollapseSelectUserId: ''
+    nowCollapseSelectUserId: '',
+    hasNextPage: false
   }),
+  computed: {
+    onNextPage() {
+      return debounce(() => {
+        this.loadNextPage()
+      }, 500)
+    }
+  },
   watch: {
     usersByRealName: {
       handler(val) {
         if (val) {
           this.userDict = {}
-          var vLen = val.length
-          for (var i = 0; i < vLen; i++) {
+          const vLen = val.length
+          for (let i = 0; i < vLen; i++) {
             this.userDict[val[i].id] = val[i]
           }
         }
       }
-    },
-    immediate: true
+    }
   },
   methods: {
-    mapUser(li) {
-      return {
-        description: li.companyName + li.dutiesName,
-        id: li.id,
-        value: li.id,
-        realName: li.realName,
-        avatar: li.avatar
-      }
+    avatarLoad(index) {
+      this.$nextTick(() => {
+        this.$emit('update:avatar', this.usersByRealName[index].avatar)
+      })
     },
     selectUserChange(id) {
       if (id) {
@@ -62,16 +76,33 @@ export default {
     loadCollapseUserAvatarRealName(id) {
       var u = this.userDict[id]
       this.$emit('change', u)
-      u.canLoadAvatar = true
+      if (u.avatar) this.$emit('update:avatar', u.avatar)
+      else u.canLoadAvatar = true
+
+      this.$message.success(`已选择:${u.realName}(${u.id})`)
     },
-    handleUserSelectByRealnameChange(val) {
-      if (!val) return (this.usersByRealName = [])
-      getUserIdByRealName(val).then(data => {
-        this.usersByRealName = data.list.map(li => {
-          li.canLoadAvatar = false
-          return li
+    handleUserSelectByRealnameChange() {
+      this.usersByRealName = []
+      if (!this.nowSelectRealName) return
+      this.nowIndex = 0
+      this.onNextPage()
+    },
+    loadNextPage() {
+      this.loading = true
+      getUserIdByRealName(this.nowSelectRealName, this.nowIndex++, 10)
+        .then(data => {
+          this.usersByRealName = this.usersByRealName.concat(
+            data.list.map(li => {
+              li.canLoadAvatar = false
+              li.avatar = null
+              return li
+            })
+          )
+          this.hasNextPage = data.totalCount > this.usersByRealName.length
         })
-      })
+        .finally(() => {
+          this.loading = false
+        })
     }
   }
 }
