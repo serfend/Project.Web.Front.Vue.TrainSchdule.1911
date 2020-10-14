44d9f72c59a7b37e1cb7f3869e463adaa3d04955
serfend <serfend@foxmail.com>
2020-10-14 19:06:27 +0800

    refactor[code-review]eslint

--- src/views/ApplyDetail/index.vue
+++ src/views/ApplyDetail/index.vue
@@ -1,123 +1,120 @@
 <template>
-<div style="padding: 10px">
-  <el-link icon="el-icon-download" type="success" @click="downloadUserApplies">导出休假登记卡</el-link>
-  <span v-if="detail && detail.id">
-    <action-examine :row="detail" style="display: inline" @updated="requestUpdate" />
-    <action-user :row="detail" style="display: inline" @updated="requestUpdate" />
-  </span>
-  <div style="padding-top: 0.5rem">
-    <el-form type="flex" label-width="8rem">
-      <div class="content-card">
-        <el-card v-if="detail && detail.id && detail.status !== 20" v-loading="loading" shadow="hover">
-          <h3 slot="header">本次休假</h3>
-          <div v-if="detail.id">
-            <el-form-item label="基本">
-              <el-tag v-if="detail.request.vacationType" effect="dark" :type="
+  <div style="padding: 10px">
+    <el-link icon="el-icon-download" type="success" @click="downloadUserApplies">导出休假登记卡</el-link>
+    <span v-if="detail && detail.id">
+      <action-examine :row="detail" style="display: inline" @updated="requestUpdate" />
+      <action-user :row="detail" style="display: inline" @updated="requestUpdate" />
+    </span>
+    <div style="padding-top: 0.5rem">
+      <el-form type="flex" label-width="8rem">
+        <div class="content-card">
+          <el-card
+            v-if="detail && detail.id && detail.status !== 20"
+            v-loading="loading"
+            shadow="hover"
+          >
+            <h3 slot="header">本次休假</h3>
+            <div v-if="detail.id">
+              <el-form-item label="基本">
+                <el-tag
+                  v-if="detail.request.vacationType"
+                  effect="dark"
+                  :type="
                     detail.request.vacationType === '正休'
                       ? 'primary'
                       : 'danger'
-                  ">{{ detail.request.vacationType }}</el-tag>
-              <div v-if="staticData.vacationStart">
-                <el-tooltip effect="light">
-                  <template slot="content">
-                    <span>{{ staticData.vacationSpent }}/{{
-                          staticData.vacationLength
-                        }}天</span>
-                  </template>
-                  <el-col v-if="staticData.vacationSpent >= 0" :lg="6" :md="12" :sm="24">
-                    <el-progress :width="100" :percentage="staticData.vacationProgress" />
-                  </el-col>
+                  "
+                >{{ detail.request.vacationType }}</el-tag>
+                <div v-if="staticData.vacationStart">
+                  <el-tooltip effect="light">
+                    <template slot="content">
+                      <span>{{ staticData.vacationSpent }}/{{ staticData.vacationLength }}天</span>
+                    </template>
+                    <el-col v-if="staticData.vacationSpent >= 0" :lg="6" :md="12" :sm="24">
+                      <el-progress :width="100" :percentage="staticData.vacationProgress" />
+                    </el-col>
 
-                  <span v-else>距离离队时间:{{ -staticData.vacationSpent }}天</span>
-                </el-tooltip>
-              </div>
-              <span v-else>
-                <el-tag v-if="statusDic[detail.status]" :color="statusDic[detail.status].color" class="white--text">{{ statusDic[detail.status].desc }}</el-tag>
-              </span>
-            </el-form-item>
-            <el-form-item label="原因">{{
-                detail.request.reason ? detail.request.reason : '未填写'
-              }}</el-form-item>
-            <el-form-item label="创建时间">{{ detail.create }}</el-form-item>
-            <el-form-item label="假期天数">
-              <span>{{
-                  `净假期${detail.request.vacationLength}天 在途${detail.request.onTripLength}天`
-                }}</span>
-              <el-tooltip v-for="a in detail.request.additialVacations" :key="a.id" :content="`开始于${parseTime(a.start)}的${a.length}天${
+                    <span v-else>距离离队时间:{{ -staticData.vacationSpent }}天</span>
+                  </el-tooltip>
+                </div>
+                <span v-else>
+                  <el-tag
+                    v-if="statusDic[detail.status]"
+                    :color="statusDic[detail.status].color"
+                    class="white--text"
+                  >{{ statusDic[detail.status].desc }}</el-tag>
+                </span>
+              </el-form-item>
+              <el-form-item label="原因">{{ detail.request.reason ? detail.request.reason : '未填写' }}</el-form-item>
+              <el-form-item label="创建时间">{{ detail.create }}</el-form-item>
+              <el-form-item label="假期天数">
+                <span>{{ `净假期${detail.request.vacationLength}天 在途${detail.request.onTripLength}天` }}</span>
+                <el-tooltip
+                  v-for="a in detail.request.additialVacations"
+                  :key="a.id"
+                  :content="`开始于${parseTime(a.start)}的${a.length}天${
                     a.name
-                  },${a.description}`">
-                <el-tag style="margin-left: 10px">{{
-                    `${a.length}天${a.name}`
-                  }}</el-tag>
-              </el-tooltip>
-            </el-form-item>
-            <el-form-item label="休假日期">
-              <span>{{ parseTime(detail.request.stampLeave) }} -
-                {{ parseTime(detail.request.stampReturn) }}</span>
-            </el-form-item>
-            <el-form-item label="休假地点">
-              <span>{{
-                  detail.request.vacationPlace &&
-                  detail.request.vacationPlace.name
-                }}</span>
-              <span v-if="detail.request.vacationPlaceName">{{
-                  `(${detail.request.vacationPlaceName})`
-                }}</span>
-            </el-form-item>
-            <el-form-item label="交通工具">
-              <span>{{
-                  detail.request.byTransportation === 0
-                    ? '火车'
-                    : detail.request.byTransportation === 1
-                    ? '飞机'
-                    : '其他'
-                }}</span>
-            </el-form-item>
-          </div>
-        </el-card>
-      </div>
-      <div class="content-card">
-        <AuditStatus :loading="loading" :data="detail" />
-      </div>
-      <div class="content-card">
-        <el-card v-if="detail && detail.id">
-          <h3 slot="header">申请人信息</h3>
-          <MyApply :id="detail.base.id" :list.sync="selfHistory" :start="null" :auto-expand="false">
-            <template slot="inner">
-              <el-card>
-                <SettleFormItem :form.sync="settle.self" disabled label="本人所在地" />
-                <SettleFormItem :form.sync="settle.lover" disabled label="配偶所在地" />
-                <SettleFormItem :form.sync="settle.parent" disabled label="父母所在地" />
-                <SettleFormItem :form.sync="settle.loversParent" disabled label="配偶父母所在地" />
-              </el-card>
-            </template>
-          </MyApply>
-        </el-card>
-      </div>
-    </el-form>
+                  },${a.description}`"
+                >
+                  <el-tag style="margin-left: 10px">{{ `${a.length}天${a.name}` }}</el-tag>
+                </el-tooltip>
+              </el-form-item>
+              <el-form-item label="休假日期">
+                <span>
+                  {{ parseTime(detail.request.stampLeave) }} -
+                  {{ parseTime(detail.request.stampReturn) }}
+                </span>
+              </el-form-item>
+              <el-form-item label="休假地点">
+                <span>{{ detail.request.vacationPlace && detail.request.vacationPlace.name }}</span>
+                <span
+                  v-if="detail.request.vacationPlaceName"
+                >{{ `(${detail.request.vacationPlaceName})` }}</span>
+              </el-form-item>
+              <el-form-item label="交通工具">
+                <span>{{ detail.request.byTransportation === 0 ? '火车' : detail.request.byTransportation === 1? '飞机': '其他' }}</span>
+              </el-form-item>
+            </div>
+          </el-card>
+        </div>
+        <div class="content-card">
+          <AuditStatus :loading="loading" :data="detail" />
+        </div>
+        <div class="content-card">
+          <el-card v-if="detail && detail.id">
+            <h3 slot="header">申请人信息</h3>
+            <MyApply
+              :id="detail.base.id"
+              :list.sync="selfHistory"
+              :start="null"
+              :auto-expand="false"
+            >
+              <template slot="inner">
+                <el-card>
+                  <SettleFormItem :form.sync="settle.self" disabled label="本人所在地" />
+                  <SettleFormItem :form.sync="settle.lover" disabled label="配偶所在地" />
+                  <SettleFormItem :form.sync="settle.parent" disabled label="父母所在地" />
+                  <SettleFormItem :form.sync="settle.loversParent" disabled label="配偶父母所在地" />
+                </el-card>
+              </template>
+            </MyApply>
+          </el-card>
+        </div>
+      </el-form>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-import {
-  detail
-} from '@/api/apply/query'
-import {
-  exportUserApplies
-} from '@/api/common/static'
-import {
-  datedifference,
-  parseTime
-} from '@/utils'
+import { detail } from '@/api/apply/query'
+import { exportUserApplies } from '@/api/common/static'
+import { datedifference, parseTime } from '@/utils'
 import ActionExamine from '../QueryAndAuditApplies/ActionExamine'
 import ActionUser from '../QueryAndAuditApplies/ActionUser'
 import SettleFormItem from '@/components/SettleFormItem'
 import AuditStatus from './components/AuditStatus'
 import MyApply from '@/views/MyApply'
-import {
-  debounce
-} from '../../utils'
+import { debounce } from '../../utils'
 export default {
   name: 'ApplyDetail',
   components: {
@@ -182,10 +179,7 @@ export default {
     },
     initstaticDataData() {
       const now = new Date()
-      const {
-        request,
-        status
-      } = this.detail
+      const { request, status } = this.detail
       const start = request.stampLeave
       const end = request.stampReturn
       const vacationLength = datedifference(end, start) + 1
--- src/views/register/index.vue
+++ src/views/register/index.vue
@@ -1,107 +1,156 @@
 <template>
-<div>
-  <el-tabs v-model="current_tab" type="border-card" class="tab-container">
-    <el-tab-pane :disabled="waitToAuthRegisterUsersLoadId == ''" name="reg" label="用户信息">
-      <el-form v-loading="submitLoading" label-position="right" label-width="120px">
-        <el-collapse v-model="nowStep" accordion>
-          <el-collapse-item v-for="opt in stepOptions.filter((i) => !i.removed)" :key="opt.index" :name="opt.index">
-            <template slot="title">
-              {{ opt.name }}
-              <i :class="opt.icon" />
-            </template>
-            <el-card :key="opt.index" shadow="hover">
-              <component :is="opt.component" :form.sync="registerForm[opt.component]" />
-            </el-card>
-          </el-collapse-item>
-        </el-collapse>
-      </el-form>
-      <el-button v-if="isToRegister" type="success" :loading="submitLoading" @click="submitRegister(true)">提交注册</el-button>
-      <el-button v-if="isToRegister" type="primary" :loading="submitLoading" @click="switchFormType">切换到审核认证</el-button>
-      <el-button v-if="!isToRegister" type="warning" :loading="submitLoading" @click="submitRegister(false)">修改信息</el-button>
-      <span v-show="
+  <div>
+    <el-tabs v-model="current_tab" type="border-card" class="tab-container">
+      <el-tab-pane :disabled="waitToAuthRegisterUsersLoadId == ''" name="reg" label="用户信息">
+        <el-form v-loading="submitLoading" label-position="right" label-width="120px">
+          <el-collapse v-model="nowStep" accordion>
+            <el-collapse-item
+              v-for="opt in stepOptions.filter((i) => !i.removed)"
+              :key="opt.index"
+              :name="opt.index"
+            >
+              <template slot="title">
+                {{ opt.name }}
+                <i :class="opt.icon" />
+              </template>
+              <el-card :key="opt.index" shadow="hover">
+                <component :is="opt.component" :form.sync="registerForm[opt.component]" />
+              </el-card>
+            </el-collapse-item>
+          </el-collapse>
+        </el-form>
+        <el-button
+          v-if="isToRegister"
+          type="success"
+          :loading="submitLoading"
+          @click="submitRegister(true)"
+        >提交注册</el-button>
+        <el-button
+          v-if="isToRegister"
+          type="primary"
+          :loading="submitLoading"
+          @click="switchFormType"
+        >切换到审核认证</el-button>
+        <el-button
+          v-if="!isToRegister"
+          type="warning"
+          :loading="submitLoading"
+          @click="submitRegister(false)"
+        >修改信息</el-button>
+        <span
+          v-show="
             waitToAuthRegisterUsersLoadId !== '' && selectIsInvalidAccount
-          ">
-        <el-button type="danger" :loading="submitLoading" @click="submitValidAccount(false)">注册信息不合格</el-button>
-        <el-button type="success" :loading="submitLoading" @click="submitValidAccount(true)">注册信息合格</el-button>
-      </span>
-    </el-tab-pane>
-    <el-tab-pane :disabled="isToRegister" name="lst" label="用户列表">
-      <el-card>
-        <div slot="header">
-          <el-card>
-            <span>选择成员</span>
-            <UserSelector :code.sync="nowSelectRealName" default-info="未选择" style="display: inline; margin: 0 1rem 0 0" @change="handleCurrentChange" />
-            <span>选择单位</span>
-            <CompanySelector v-model="nowSelectCompany" placeholder="选择需要检查的单位" style="width: 40%" />
-            <div style="margin-top: 0.5rem">
-              <el-button type="primary" :loading="submitLoading" @click="requireLoadWaitToAUthRegisterUsers">刷新待认证人员</el-button>
-              <el-button v-if="!isToRegister" type="primary" :loading="submitLoading" @click="switchFormType">切换到普通注册</el-button>
-            </div>
-          </el-card>
-        </div>
+          "
+        >
+          <el-button
+            type="danger"
+            :loading="submitLoading"
+            @click="submitValidAccount(false)"
+          >注册信息不合格</el-button>
+          <el-button
+            type="success"
+            :loading="submitLoading"
+            @click="submitValidAccount(true)"
+          >注册信息合格</el-button>
+        </span>
+      </el-tab-pane>
+      <el-tab-pane :disabled="isToRegister" name="lst" label="用户列表">
+        <el-card>
+          <div slot="header">
+            <el-card>
+              <span>选择成员</span>
+              <UserSelector
+                :code.sync="nowSelectRealName"
+                default-info="未选择"
+                style="display: inline; margin: 0 1rem 0 0"
+                @change="handleCurrentChange"
+              />
+              <span>选择单位</span>
+              <CompanySelector
+                v-model="nowSelectCompany"
+                placeholder="选择需要检查的单位"
+                style="width: 40%"
+              />
+              <div style="margin-top: 0.5rem">
+                <el-button
+                  type="primary"
+                  :loading="submitLoading"
+                  @click="requireLoadWaitToAUthRegisterUsers"
+                >刷新待认证人员</el-button>
+                <el-button
+                  v-if="!isToRegister"
+                  type="primary"
+                  :loading="submitLoading"
+                  @click="switchFormType"
+                >切换到普通注册</el-button>
+              </div>
+            </el-card>
+          </div>
 
-        <el-table v-loading="tableLoading" :data="waitToAuthRegisterUsers" highlight-current-row style="width: 100%" @current-change="handleCurrentChange">
-          <el-table-column label="姓名" width="100">
-            <template slot-scope="scope">
-              <el-popover placement="right-start" trigger="hover" @show="scope.row.userHasShow = true">
-                <User :data="scope.row" :can-load-avatar="scope.row.userHasShow" />
-                <span slot="reference">{{ scope.row.realName }}</span>
-              </el-popover>
-            </template>
-          </el-table-column>
-          <el-table-column prop="dutiesName" label="职务" width="150" />
-          <el-table-column prop="vacation.yearlyLength" label="基础假" width="50" />
-          <el-table-column prop="vacation.maxTripTimes" label="可休路途次数" width="50" />
-          <el-table-column prop="accountAuthStatus" label="状态" width="100">
-            <template slot-scope="scope">
-              <el-tag :type="
-                    scope.row.accountAuthStatus == 1
-                      ? 'success'
-                      : scope.row.accountAuthStatus == 0
-                      ? 'info'
-                      : 'danger'
-                  ">{{
-                    scope.row.accountAuthStatus == 1
-                      ? '已认证'
-                      : scope.row.accountAuthStatus == 0
-                      ? '待认证'
-                      : '已退回'
-                  }}</el-tag>
-            </template>
-          </el-table-column>
-          <el-table-column prop="vacation.description" label="休假详情描述">
-            <template slot-scope="scope">
-              <span slot="reference">{{
-                  scope.row.vacation.description
-                }}</span>
-            </template>
-          </el-table-column>
-          <el-table-column prop="vacation.description" label="休假率">
-            <template slot-scope="scope">
-              <VacationDescription :users-vacation="scope.row.vacation" />
-            </template>
-          </el-table-column>
-        </el-table>
-      </el-card>
-      <Pagination v-if="!isToRegister" :pagesetting.sync="MembersQuery" :total-count="MembersQueryTotalCount" />
-    </el-tab-pane>
-  </el-tabs>
-  <el-dialog :visible="init_page_over && not_login_show">
-    <SvgIcon icon-class="Message_error" style-normal="width:6rem;height:6rem;margin: auto;color:rgb(243, 126, 125)" class-name="item-put-center" />
-    <div class="item-put-center" style="margin: 3rem 0 2rem 0; font-size: 2rem">
-      进入注册页面失败
-    </div>
-    <div class="item-put-center" style="margin: 3rem 0 2rem 0; font-size: 1rem">
-      为保证您的账号安全
-    </div>
+          <el-table
+            v-loading="tableLoading"
+            :data="waitToAuthRegisterUsers"
+            highlight-current-row
+            style="width: 100%"
+            @current-change="handleCurrentChange"
+          >
+            <el-table-column label="姓名" width="100">
+              <template slot-scope="scope">
+                <el-popover
+                  placement="right-start"
+                  trigger="hover"
+                  @show="scope.row.userHasShow = true"
+                >
+                  <User :data="scope.row" :can-load-avatar="scope.row.userHasShow" />
+                  <span slot="reference">{{ scope.row.realName }}</span>
+                </el-popover>
+              </template>
+            </el-table-column>
+            <el-table-column prop="dutiesName" label="职务" width="150" />
+            <el-table-column prop="vacation.yearlyLength" label="基础假" width="50" />
+            <el-table-column prop="vacation.maxTripTimes" label="可休路途次数" width="50" />
+            <el-table-column prop="accountAuthStatus" label="状态" width="100">
+              <template slot-scope="scope">
+                <el-tag
+                  :type="scope.row.accountAuthStatus == 1?'success': scope.row.accountAuthStatus == 0?'info':'danger'"
+                >{{ scope.row.accountAuthStatus == 1 ? '已认证' : scope.row.accountAuthStatus == 0 ? '待认证' : '已退回' }}</el-tag>
+              </template>
+            </el-table-column>
+            <el-table-column prop="vacation.description" label="休假详情描述">
+              <template slot-scope="scope">
+                <span slot="reference">{{ scope.row.vacation.description }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column prop="vacation.description" label="休假率">
+              <template slot-scope="scope">
+                <VacationDescription :users-vacation="scope.row.vacation" />
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-card>
+        <Pagination
+          v-if="!isToRegister"
+          :pagesetting.sync="MembersQuery"
+          :total-count="MembersQueryTotalCount"
+        />
+      </el-tab-pane>
+    </el-tabs>
+    <el-dialog :visible="init_page_over && not_login_show">
+      <SvgIcon
+        icon-class="Message_error"
+        style-normal="width:6rem;height:6rem;margin: auto;color:rgb(243, 126, 125)"
+        class-name="item-put-center"
+      />
+      <div class="item-put-center" style="margin: 3rem 0 2rem 0; font-size: 2rem">进入注册页面失败</div>
+      <div class="item-put-center" style="margin: 3rem 0 2rem 0; font-size: 1rem">为保证您的账号安全</div>
 
-    <div class="item-put-center" style="margin: 3rem 0 2rem 0; font-size: 1.2rem">
-      <span>请</span>
-      <el-link href="/" type="danger">登录任何一名其他成员的账号</el-link>
-      <span>执行注册操作</span>
-    </div>
-  </el-dialog>
-</div>
+      <div class="item-put-center" style="margin: 3rem 0 2rem 0; font-size: 1.2rem">
+        <span>请</span>
+        <el-link href="/" type="danger">登录任何一名其他成员的账号</el-link>
+        <span>执行注册操作</span>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -117,25 +166,12 @@ import UserSelector from '@/components/User/UserSelector'
 
 import CompanySelector from '@/components/Company/CompanySelector'
 import VacationDescription from '@/components/Vacation/VacationDescription'
-import {
-  regnew,
-  authUserRegister,
-  modefyUser
-} from '@/api/account'
-import {
-  getMembers
-} from '@/api/company'
-import {
-  getUsersVacationLimit,
-  getUserAvatar
-} from '@/api/user/userinfo'
-import {
-  getUserAllInfo
-} from '@/api/user/usercompany'
+import { regnew, authUserRegister, modefyUser } from '@/api/account'
+import { getMembers } from '@/api/company'
+import { getUsersVacationLimit, getUserAvatar } from '@/api/user/userinfo'
+import { getUserAllInfo } from '@/api/user/usercompany'
 import Pagination from '@/components/Pagination'
-import {
-  debounce
-} from '@/utils'
+import { debounce } from '@/utils'
 
 export default {
   name: 'Register',
@@ -161,7 +197,8 @@ export default {
       current_tab: 'lst',
       registerForm: this.createForm(),
       nowStep: 1,
-      stepOptions: [{
+      stepOptions: [
+        {
           name: '基本',
           index: 1,
           icon: 'el-icon-s-custom',
@@ -226,7 +263,7 @@ export default {
     currentCmp: {
       handler(val) {
         this.nowSelectCompany = {
-          code: val
+          code: val,
         }
         this.not_login_show = !val
         const to_tab = this.isToRegister ? 'reg' : 'lst'
@@ -301,7 +338,8 @@ export default {
       var actionName = valid ? '认证' : '退回'
       this.$confirm(
         `即将对用户${this.registerForm.Base.realName}(${this.waitToAuthRegisterUsersLoadId})的注册进行【${actionName}】操作`,
-        `${actionName}提示`, {
+        `${actionName}提示`,
+        {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning',
@@ -327,11 +365,11 @@ export default {
       this.tableLoading = true
       this.submitLoading = true
       getMembers({
-          code: this.nowSelectCompany.code,
-          page: this.MembersQuery.pageIndex,
-          pageSize: this.MembersQuery.pageSize,
-        })
-        .then(async (data) => {
+        code: this.nowSelectCompany.code,
+        page: this.MembersQuery.pageIndex,
+        pageSize: this.MembersQuery.pageSize,
+      })
+        .then(async(data) => {
           this.MembersQueryTotalCount = data.totalCount
           this.waitToAuthRegisterUsers = this.loadUserList(data.list)
           await this.loadSingleUser()
@@ -348,9 +386,9 @@ export default {
           new Promise((resolve, reject) => {
             const item = this.waitToAuthRegisterUsers[i]
             return Promise.all([
-                getUserAvatar(item.id),
-                getUsersVacationLimit(item.id),
-              ])
+              getUserAvatar(item.id),
+              getUsersVacationLimit(item.id),
+            ])
               .then(([avatar, vacation]) => {
                 item.avatar = avatar.url
                 item.vacation = vacation
@@ -405,9 +443,7 @@ export default {
             },
             titleDate: duties.titleDate,
           }
-          const {
-            inviteBy
-          } = data.application
+          const { inviteBy } = data.application
           this.selectIsInvalidAccount = this.checkUserValid(inviteBy)
           this.nowSelectCompany = company
         })
