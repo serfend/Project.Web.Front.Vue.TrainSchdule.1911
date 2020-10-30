701b27717916ea47ff54c6d08d6e7bf5323eb2d1
serfend <serfend@foxmail.com>
2020-10-30 08:00:40 +0800

    feat[clipboard]show tips if not support

--- src/views/common/ClipboardMonitor/index.vue
+++ src/views/common/ClipboardMonitor/index.vue
@@ -11,6 +11,7 @@ export default {
     clip_board_watching: null,
     urlKey: null,
     wait_to_show: false,
+    do_not_show_tip_enable: false,
   }),
   computed: {
     short_url_loading() {
@@ -44,9 +45,27 @@ export default {
   },
   methods: {
     check_clipboard() {
+      if (!navigator || !navigator.clipboard || !navigator.clipboard.readText) {
+        this.show_clipboard_notsupport_tip()
+        return
+      }
       navigator.clipboard.readText().then((c) => {
         this.$store.dispatch('app/checkClipboard', c)
       })
+    },
+    show_clipboard_notsupport_tip() {
+      if (this.do_not_show_tip_enable) return
+      this.$notify({
+        title: '不支持剪切板',
+        message:
+          '将无法使用休假分享功能,建议使用chrome66/firefox63以上版本的浏览器。',
+        duration: 10000,
+        type: 'error',
+        onClose: this.do_not_show_tip,
+      })
+    },
+    do_not_show_tip() {
+      this.do_not_show_tip_enable = true
     },
   },
 }
