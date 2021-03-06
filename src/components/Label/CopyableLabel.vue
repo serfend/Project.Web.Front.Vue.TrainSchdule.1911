6a91c9c3ef661e980b14001c8e2781f3d81fadd9
汉广 <serfend@foxmail.com>
2021-03-06 14:42:21 +0800

    feat[component]label

---
+++ src/components/Label/CopyableLabel.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-tooltip effect="light">
+    <template #content>
+      <div style="text-align:center">
+        <div style="font-size:0.8rem;">↑点击复制</div>
+        <span>{{ label }}</span>
+      </div>
+    </template>
+    <el-button type="text" @click="clipBoard(content||label,$event)">{{ label }}</el-button>
+  </el-tooltip>
+</template>
+
+<script>
+import clipBoard from '@/utils/clipboard'
+export default {
+  name: 'CopyableLabel',
+  props: {
+    label: {
+      type: String,
+      default: null
+    },
+    content: {
+      type: String,
+      default: null
+    }
+  },
+  methods: {
+    clipBoard
+  }
+}
+</script>
