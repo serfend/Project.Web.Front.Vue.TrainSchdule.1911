78674352d9774713c3848b0bb1161ec505ef073a
汉广 <serfend@foxmail.com>
2021-04-11 19:13:20 +0800

    refactor[account]merge forget-psw and restore-account

--- src/router/modules/application_setting/modules/user_manage/UserRestore.js
+++ src/router/modules/application_setting/modules/user_manage/UserRestore.js
@@ -1,6 +1,6 @@
 const route = {
   path: 'user-restore',
-  component: () => import('@/views/register/restore'),
+  component: () => import('@/views/ForgetPassword'),
   name: 'UsersRestore',
   meta: {
     ctitle: '账号恢复',
--- src/views/ForgetPassword/index.vue
+++ src/views/ForgetPassword/index.vue
@@ -1,41 +1,159 @@
 <template>
   <div>
-    <el-card header="找回账号">
+    <el-card>
+      <template #header>
+        <h2>找回账号</h2>
+      </template>
       <div class="page-container">
-        <div class="warning">
-          <p>此页面用于找回用户名或密码</p>
-          <p>
-            <code>密码</code>丢失可以通过本人或上级的授权码找回
-          </p>
-          <p>遗忘了用户名可以通过两种方式找回:1.依据姓名找回 2.依据单位找回</p>
-        </div>
-        <el-col :lg="8" :md="12" :sm="24">
-          <el-card header="找回密码">
-            <ResetPassword :old-psw-input="false" />
-          </el-card>
-        </el-col>
-        <el-col :lg="8" :md="12" :sm="24">
-          <el-card header="找回账号 - 通过姓名">
-            <FindUserByRealName />
-          </el-card>
-        </el-col>
-        <el-col :lg="8" :md="12" :sm="24">
-          <el-card header="找回账号 - 通过单位">
-            <FindUserByCompany />
-          </el-card>
-        </el-col>
+        <el-collapse v-model="activePannel" accordion>
+          <el-collapse-item name="0" title="说明">
+            <div class="warning">
+              <p>此页面用于找回用户名或密码</p>
+              <p>1. 密码丢失可以通过本人或上级的授权码找回</p>
+              <p>2. 遗忘了用户名可以通过两种方式找回:1.依据姓名找回 2.依据单位找回</p>
+              <p>3. 账号被删除后可通过管理员在此处操作恢复</p>
+            </div>
+          </el-collapse-item>
+          <el-collapse-item name="1" title="用户查找">
+            <el-row :gutter="20">
+              <el-col :lg="8" :md="12" :sm="24">
+                <el-card header="找回账号 - 通过姓名">
+                  <FindUserByRealName />
+                </el-card>
+              </el-col>
+              <el-col :lg="8" :md="12" :sm="24">
+                <el-card header="找回账号 - 通过单位">
+                  <FindUserByCompany />
+                </el-card>
+              </el-col>
+            </el-row>
+          </el-collapse-item>
+          <el-collapse-item name="2" title="账号恢复和找回密码">
+            <el-row :gutter="20">
+              <el-col :lg="8" :md="12" :sm="24">
+                <el-card header="找回密码">
+                  <ResetPassword :old-psw-input="false" />
+                </el-card>
+              </el-col>
+              <el-col :lg="8" :md="12" :sm="24">
+                <el-card>
+                  <div slot="header">
+                    <span style="font-size:1.5rem;font-weight:600">恢复账号提交</span>
+                    <el-popover trigger="hover">
+                      <div style="width:12rem">
+                        <p>新恢复的账号将出现在其原所属单位，并恢复其原来的信息。</p>
+                        <p>被删除的休假记录也会一并被恢复，如需继续删除休假记录，需手动操作。</p>
+                      </div>
+                      <i slot="reference" class="el-icon-info blue--text" />
+                    </el-popover>
+                  </div>
+                  <el-form label-width="5rem">
+                    <el-alert
+                      v-if="account.id && account.id.length>5"
+                      :type="is_removed?'success':'error'"
+                      style="margin-bottom:1rem"
+                    >{{ errorinfo }}</el-alert>
+                    <el-form-item label="账号">
+                      <el-input v-model="account.id" placeholder="输入已被删除的账号" />
+                    </el-form-item>
+                    <el-form-item label="恢复原因">
+                      <el-input v-model="account.reason" type="textarea" rows="5" />
+                    </el-form-item>
+                    <AuthCode v-model="account.auth" />
+                    <el-button
+                      v-loading="loading"
+                      :disabled="!is_removed"
+                      type="success"
+                      style="margin-top:1rem;width:100%"
+                      @click="restore_account"
+                    >恢复</el-button>
+                  </el-form>
+                </el-card>
+              </el-col>
+            </el-row>
+          </el-collapse-item>
+        </el-collapse>
       </div>
     </el-card>
   </div>
 </template>
 
 <script>
+import { restoreAccount } from '@/api/account'
+import { getUserBase } from '@/api/user/userinfo'
+import { debounce } from '@/utils'
 import ResetPassword from '@/components/ResetPassword'
 import FindUserByRealName from '@/components/User/FindUserByRealName'
 import FindUserByCompany from '@/components/User/FindUserByCompany'
 export default {
   name: 'ForgetPassword',
-  components: { ResetPassword, FindUserByRealName, FindUserByCompany }
+  components: {
+    ResetPassword,
+    FindUserByRealName,
+    FindUserByCompany,
+    AuthCode: () => import('@/components/AuthCode')
+  },
+  data: () => ({
+    activePannel: '0',
+    account: {
+      auth: {},
+      id: '',
+      reason: ''
+    },
+    loading: false,
+    is_removed: true
+  }),
+  computed: {
+    errorinfo() {
+      return this.is_removed
+        ? '当前账号处于不存在状态，可尝试恢复。'
+        : '当前账号存在，不可恢复'
+    },
+    requireCheck() {
+      return debounce(() => {
+        this.check()
+      }, 300)
+    }
+  },
+  watch: {
+    'account.id': {
+      handler(val) {
+        this.$nextTick(() => {
+          this.check()
+        })
+      }
+    }
+  },
+  methods: {
+    restoreAccount,
+    check() {
+      this.loading = true
+      getUserBase(this.account.id, true)
+        .then(data => {
+          this.is_removed = !data || !data.base || !data.base.cid
+        })
+        .catch(e => {
+          this.is_removed = true
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    },
+    restore_account() {
+      this.loading = true
+      const data = Object.assign({}, this.account)
+      delete data.auth
+      const auth = this.account.auth
+      restoreAccount({ auth, data })
+        .then(() => {
+          this.$message.success('已成功恢复')
+        })
+        .finally(() => {
+          this.loading = false
+          this.check()
+        })
+    }
+  }
 }
 </script>
 
