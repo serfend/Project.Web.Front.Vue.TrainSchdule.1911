4f76ea047020abb66b8a18ef4f5e1a1464b63f67
汉广 <serfend@foxmail.com>
2021-06-12 22:39:55 +0800

    fix[apply-new]transportation types

--- src/components/Vacation/TransportationType/types.js
+++ src/components/Vacation/TransportationType/types.js
@@ -4,6 +4,8 @@ export default {
   2: ['bus', '公交'],
   3: ['taxi', '出租车/网约车'],
   6: ['qiche', '私家车'],
+  12: ['sport', '步行'],
   8: ['beijing-metro', '地铁'],
+  15: ['warn', '其他'],
   default: ['guide', '未知']
 }
