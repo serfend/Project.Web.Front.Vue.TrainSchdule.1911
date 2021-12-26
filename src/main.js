7bc7c234805b1401b9ed2c52dee0514b59a7fce5
汉广 <serfend@foxmail.com>
2021-12-26 11:01:25 +0800

    chore[element]use default styles

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import 'normalize.css/normalize.css' // a modern alternative to CSS resets
 import Element from 'element-ui'
 import '@/styles/index.scss' // global css
 import './styles/element-variables.scss'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium', // set element-ui default size
   i18n: (key, value) => i18n.t(key, value)
