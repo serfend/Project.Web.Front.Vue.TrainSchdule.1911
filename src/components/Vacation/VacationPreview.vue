0db1e8e0dc784128fb21c8679f7287677ff60e6c
汉广 <serfend@foxmail.com>
2022-03-31 18:53:57 +0800

    feat[apply-new]audit-stream move to submit form

--- src/components/Vacation/VacationPreview.vue
+++ src/components/Vacation/VacationPreview.vue
@@ -1,12 +1,18 @@
 <template>
-  <el-card :header="`${extractEntityType(entityType)}审批流程预览：${solutionName}`">
+  <el-card :header="`${extractEntityType(entityType)}审批流程预览：${solutionName||''}`">
     <ApplyAuditStreamPreview
+      v-show="!validateInfoInner"
       :solution-name.sync="solutionName"
       :userid="userid"
       :entity-type="entityType"
       :entity-type-desc="entityTypeDesc"
       :show-detail="true"
+      :validate-info.sync="validateInfoInner"
     />
+    <div v-show="validateInfoInner">
+      <span>当前审批流程加载失败：</span>
+      <el-tag type="danger">{{ validateInfoInner }}</el-tag>
+    </div>
   </el-card>
 </template>
 
@@ -18,11 +24,22 @@ export default {
   props: {
     userid: { type: String, default: null },
     entityType: { type: String, default: 'vacation' },
-    entityTypeDesc: { type: String, default: null }
+    entityTypeDesc: { type: String, default: null },
+    validateInfo: { type: String, default: null }
   },
   data: () => ({
-    solutionName: null
+    solutionName: null,
+    validate_info: null
   }),
+  computed: {
+    validateInfoInner: {
+      get() { return this.validate_info },
+      set(v) {
+        this.validate_info = v
+        this.$emit('update:validateInfo', v)
+      }
+    }
+  },
   methods: {
     extractEntityType(v) {
       return { vacation: '休假', inday: '请假' }[v]
