f6580616080090b867d7c5939df2feeca444df2f
汉广 <serfend@foxmail.com>
2023-01-09 10:56:02 +0800

    feat[travel-list]detail info

--- src/components/Vehicle/TravelList.vue
+++ src/components/Vehicle/TravelList.vue
@@ -1,24 +1,47 @@
 <template>
   <div>
-    <h3>目标{{ id }}详细记录</h3>
     <el-table :data="innerList">
-      <el-table-column label="目标">
-        <template slot-scope="{ row }">{{ row.name }}</template>
-      </el-table-column>
       <el-table-column label="驾驶员">
-        <template slot-scope="{ row }">{{ row.driver }}</template>
-      </el-table-column>
-      <el-table-column label="车辆">
-        <template slot-scope="{ row }">{{ row.vehicle }}</template>
-      </el-table-column>
-      <el-table-column label="出车时间">
         <template slot-scope="{ row }">
-          <el-tool-tip :content="formatTime(row.timeStart)">{{ parseTime(row.timeStart) }}</el-tool-tip>
-          <span>-</span>
-          <el-tool-tip :content="formatTime(row.timeEnd)">{{ parseTime(row.timeEnd) }}</el-tool-tip>
+          <div v-if="row.driver">
+            <UserFormItem :data="row.driver.user" />
+          </div>
+          <div v-else>无</div>
         </template>
       </el-table-column>
-      <el-table-column label="乘车人数">
+      <el-table-column label="驾龄" min-width="60">
+        <template slot-scope="{ row }">
+          <div v-if="row.driver">
+            <div v-if="row.driver.timeDrive">
+              <el-tooltip :content="`驾驶时间:${parseTime(row.driver.timeDrive)}`">
+                <span>{{ formatTime(row.driver.timeDrive,'{y}-{m}-{d}') }}</span>
+              </el-tooltip>
+            </div>
+            <div v-if="row.driver.timeWorkCurrentCompany">
+              <el-tooltip :content="`本单位驾驶时间:${parseTime(row.driver.timeWorkCurrentCompany)}`">
+                <span>{{ formatTime(row.driver.timeWorkCurrentCompany,'{y}-{m}-{d}') }}</span>
+              </el-tooltip>
+            </div>
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column label="出车车辆">
+        <template slot-scope="{ row }">
+          <VehicleFormItem :data="row.vehicle" />
+        </template>
+      </el-table-column>
+      <el-table-column label="出车时间" min-width="120">
+        <template slot-scope="{ row }">
+          <el-tooltip :content="parseTime(row.timeStart)">
+            <span>{{ formatTime(row.timeStart,'{y}-{m}-{d}') }}</span>
+          </el-tooltip>
+          <span>-</span>
+          <el-tooltip :content="parseTime(row.timeEnd)">
+            <span>{{ formatTime(row.timeEnd,'{y}-{m}-{d}') }}</span>
+          </el-tooltip>
+        </template>
+      </el-table-column>
+      <el-table-column label="人数">
         <template slot-scope="{ row }">{{ row.passengerCount }}</template>
       </el-table-column>
       <el-table-column label="距离">
@@ -26,14 +49,16 @@
       </el-table-column>
       <el-table-column label="路径">
         <template slot-scope="{ row }">
-          <div v-if="row.wayPathObj">
-            <el-popover v-if="row.wayPathObj.length>2">
-              <el-steps :active="0" align-center>
-                <el-step v-for="i,index in row.wayPathObj" :key="index" :title="i.alias" description="" />
+          <div v-if="row.wayPath">
+            <el-popover v-if="row.wayPath.length>2">
+              <el-steps :active="row.wayPath.length" align-center>
+                <el-step v-for="i,index in row.wayPath" :key="index" :title="i.shortName" :description="i.name" />
               </el-steps>
-              <el-button slot="reference" type="text">共{{ row.wayPathObj.length }}个地点</el-button>
+              <el-button slot="reference" type="text">共{{ row.wayPath.length }}个地点</el-button>
             </el-popover>
-            <div v-else>{{ row.wayPathObj.map(i=>i.alias).join('-') }}</div>
+            <el-tooltip v-else :content="row.wayPath.map(i=>i.name).join('-')">
+              <span>{{ row.wayPath.map(i=>i.shortName).join('-') }}</span>
+            </el-tooltip>
             <div />
           </div>
 
@@ -56,12 +81,14 @@
 
 <script>
 import { getTravelList } from '@/api/vehicle'
-import { parseTime, formatTime } from '@/utils'
+import { parseTime, formatTime, datedifference } from '@/utils'
 import { locations as getLocations } from '@/api/common/static'
 export default {
   name: 'TravelList',
   components: {
-    Pagination: () => import('@/components/Pagination')
+    Pagination: () => import('@/components/Pagination'),
+    UserFormItem: () => import('@/components/User/UserFormItem'),
+    VehicleFormItem: () => import('./VehicleFormItem'),
   },
   props: {
     id: { type: String, required: true }
@@ -83,7 +110,7 @@ export default {
   },
   mounted() {},
   methods: {
-    parseTime, formatTime,
+    parseTime, formatTime, datedifference,
     getSplitContent (r) {
       return r && r.split('##').filter(x => x)
     },
