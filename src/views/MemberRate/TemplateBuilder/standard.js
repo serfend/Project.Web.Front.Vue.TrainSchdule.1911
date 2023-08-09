b3800ab46cb42f0d28d7dfcc16b3c3cd4b45567c
汉广 <serfend@foxmail.com>
2023-08-09 19:57:56 +0800

    refactor[member-rate]use standard handler

--- src/views/MemberRate/TemplateBuilder/standard.js
+++ src/views/MemberRate/TemplateBuilder/standard.js
@@ -1,20 +1,6 @@
-import { formatTime } from '@/utils'
+import { dataToStandard } from '@/utils/form'
 export function templateToStandard(data, ratingTypeCycleDesc, ratingTypeItem) {
-  const m = new Date().getMonth() + 1
-  data.createDate = `${m}月`
-  data.exportDate = `${m}月${new Date().getDate()}日`
-  let index = 0
-  data.list = data.list.map(i => {
-    index++
-    i.index = index
-    const u = i.user
-    if (u) {
-      const f = formatTime(u.userTitleDate, '{y}.{m}')
-      u.titleAndDate = `${u.userTitle}\n${f}`
-      u.companyAndDuty = `${u.companyName}${u.dutiesName}`
-    }
-    return i
-  })
+  data = dataToStandard(data)
   const ratingType = ratingTypeItem[0]
   const valid = ratingTypeCycleDesc && ratingType
   const prefix = valid
